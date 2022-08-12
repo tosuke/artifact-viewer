068bda0dbfd241da9c2fafbd5c89775b00bca0ec
tosuke <13393900+tosuke@users.noreply.github.com>
2022-08-12 15:55:58 +0900

    test: update zip test

--- src/lib/zip.test.ts
+++ src/lib/zip.test.ts
@@ -1,39 +1,53 @@
-import { expect, test } from "vitest";
+import { beforeEach, describe, expect, test } from "vitest";
 import { readFile } from "node:fs/promises";
 
 import { ZipReader } from "./zip";
 
-test("read zip file", async () => {
-  const body = await readFile("src/fixture/test.zip").then(
-    (buf) => buf.buffer as ArrayBuffer
-  );
+describe("ZipReader", (test) => {
+  let reader: ZipReader;
 
-  const reader = new ZipReader(new DataView(body));
+  beforeEach(async () => {
+    const body = await readFile("src/fixture/test.zip").then(
+      (buf) => buf.buffer as ArrayBuffer
+    );
+    reader = new ZipReader(new DataView(body));
+  });
 
-  expect(reader.files.length).toBe(3);
-  expect(reader.files.map((f) => f.fileName)).toMatchInlineSnapshot(`
-    [
-      "assets/",
-      "assets/index.86ef73a2.js",
-      "index.html",
-    ]
-  `);
+  test("list files", () => {
+    expect(reader.files.map((f) => f.fileName)).toMatchInlineSnapshot(`
+      [
+        "assets/",
+        "assets/index.86ef73a2.js",
+        "index.html",
+      ]
+    `);
+  });
 
-  const content = reader.files[2].decompress();
-  expect(new TextDecoder().decode(content)).toMatchInlineSnapshot(`
-    "<!DOCTYPE html>
-    <html lang=\\"en\\">
-      <head>
-        <meta charset=\\"UTF-8\\" />
-        <meta name=\\"viewport\\" content=\\"width=device-width,initial-scale=1.0\\" />
-        <title>Fixture Page</title>
-        <script type=\\"module\\" crossorigin src=\\"./assets/index.86ef73a2.js\\"></script>
-      </head>
-      <body>
-        <div id=\\"app\\"></div>
-        
-      </body>
-    </html>
-    "
-  `);
+  test("decompress file", () => {
+    const file = reader.files[2];
+
+    expect(new TextDecoder().decode(file.decompress())).toMatchInlineSnapshot(
+      `
+      "<!DOCTYPE html>
+      <html lang=\\"en\\">
+        <head>
+          <meta charset=\\"UTF-8\\" />
+          <meta name=\\"viewport\\" content=\\"width=device-width,initial-scale=1.0\\" />
+          <title>Fixture Page</title>
+          <script type=\\"module\\" crossorigin src=\\"./assets/index.86ef73a2.js\\"></script>
+        </head>
+        <body>
+          <div id=\\"app\\"></div>
+          
+        </body>
+      </html>
+      "
+    `
+    );
+  });
+
+  test("detect which is a directory or not", () => {
+    expect(reader.files[0].isDirectory).toBe(true);
+    expect(reader.files[1].isDirectory).toBe(false);
+  });
 });
