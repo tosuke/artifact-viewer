dfe8446d8723963d63a9bf1ab8535680b13c69c8
tosuke <13393900+tosuke@users.noreply.github.com>
2022-08-13 17:30:19 +0900

    feat: add edge function to resolve artifact URL

---
+++ api/resolve.ts
@@ -0,0 +1,16 @@
+export const config = {
+  runtime: "experimental-edge",
+};
+
+const handler = async (request: Request) => {
+  const url = new URL(request.url);
+  const owner = url.searchParams.get("owner");
+  const repo = url.searchParams.get("repo");
+  const id = url.searchParams.get("id");
+
+  return fetch(
+    `https://nightly.link/${owner}/${repo}/actions/artifacts/${id}/zip`
+  );
+};
+
+export default handler;
